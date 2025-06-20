8901683bf84b925e9bd490b616182ca6c3bc501e
Andres Licona Mahcado <andresmauricio745@gmail.com>
2025-06-20 09:55:45 -0500

    limpieza de datos

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,8 @@
 import ViewContainer from "@/shared/components/ViewContainer";
-import { Text, TouchableOpacity, View } from "react-native";
-import { Quemada } from "@/shared/modules/reactHomeModule/VistaQuemada/Quemada";
+import { View } from "react-native";
 import { ReactApp } from "@/shared/modules/reactHomeModule/ReactWeb/ReactApp";
-import { useContext } from "react";
-import { useAuth } from "@/shared/context/Auth";
-import { useRouter } from "expo-router";
 
 export default function TabOneScreen() {
-  const { logout,isAuthenticated } = useAuth();
-  const router = useRouter();
-
-  const handleLogout = () => {
-    logout();
-    console.log('isAuthenticated',isAuthenticated);
-    router.replace("/(loginStack)/welcome");
-  };
-
   return (
     <ViewContainer>
       <View className="flex-1 w-full h-full items-center justify-center">
